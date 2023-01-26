8d1bf5e6c0d5e3a232aeecd05a8f8b1178e7a64a
Mustafa Karaca <mustafasswork@gmail.com>
2023-01-26 16:19:15 +0300

    v1

---
+++ chatGpt/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenAI.GPT3.Managers;
+using OpenAI.GPT3;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var openAiService = new OpenAIService(new OpenAiOptions()
+        {
+            ApiKey = "YOUR_API_KEY"
+        });
+        
+
+        while (true)
+        {
+            Console.Write("Question : ");
+            var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = Console.ReadLine(),
+                Model = Models.TextDavinciV3,
+                MaxTokens = 500
+            });
+
+            if (completionResult.Successful)
+            {
+                string answer = completionResult.Choices[0].Text.Remove(0,1);
+                Console.WriteLine($"Answer :{answer}");
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                {
+                    throw new Exception("Unknown Error");
+                }
+                Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+            }
+        }
+    }
+}
